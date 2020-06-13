4d6e44e611addca850f8f1393b8885b7cdd8943c
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-06-13 17:10:59 -0300

    adjust layout add button and labels

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblArquivoSelecionado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seja bem vindo.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(347, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Clique no botão abaixo e selecione o arquivo para começar a converter.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Selecionar Arquivo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblArquivoSelecionado
+            // 
+            this.lblArquivoSelecionado.AutoSize = true;
+            this.lblArquivoSelecionado.Location = new System.Drawing.Point(12, 106);
+            this.lblArquivoSelecionado.Name = "lblArquivoSelecionado";
+            this.lblArquivoSelecionado.Size = new System.Drawing.Size(143, 13);
+            this.lblArquivoSelecionado.TabIndex = 4;
+            this.lblArquivoSelecionado.Text = "nenhum arquivo selecionado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(475, 188);
+            this.Controls.Add(this.lblArquivoSelecionado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblArquivoSelecionado;
     }
 }
 
